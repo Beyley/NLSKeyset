b0f4f2e6c07746baf80e65ea1b94d5618d3364ee
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-04-12 17:51:34 -0700

    Implement most of the rest of the chords

--- NLSKeyset/Program.cs
+++ NLSKeyset/Program.cs
@@ -14,6 +14,8 @@ public static class Program {
 	private static KeyCode _Keycode3;
 	private static KeyCode _Keycode4;
 	private static KeyCode _Keycode5;
+	private static KeyCode _KeycodeLeft;
+	private static KeyCode _KeycodeMiddle;
 
 	private static KeyCode _KeycodeDisable;
 	private static KeyCode _KeycodeEnable;
@@ -53,13 +55,13 @@ public static class Program {
 		if (!_Enabled) {
 			_ToTrigger = 0;
 		}
-		else if(bit <= 4) {
+		else if(bit != 5) {
 			if (_KeyState > origState)
 				_ToTrigger = _KeyState;
 
-			if (_KeyState == 0) {
-				//Make sure we only pass the last 5 bits (the 5 key positions)
-				TriggerChord((byte)(_ToTrigger & 0b00011111));
+			if ((_KeyState & 0b00011111) == 0 && bit < 5) {
+				//Mask out the control key
+				TriggerChord((byte)(_ToTrigger & 0b11011111));
 			}
 		}
 	}
@@ -101,12 +103,12 @@ public static class Program {
 		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode3, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
 		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode4, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
 		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode5, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
-		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode1, MASK | KeyButtonMask.ShiftMask, _XWindow, false, GrabMode.Async, GrabMode.Async);
-		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode2, MASK | KeyButtonMask.ShiftMask, _XWindow, false, GrabMode.Async, GrabMode.Async);
-		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode3, MASK | KeyButtonMask.ShiftMask, _XWindow, false, GrabMode.Async, GrabMode.Async);
-		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode4, MASK | KeyButtonMask.ShiftMask, _XWindow, false, GrabMode.Async, GrabMode.Async);
-		Xlib.XGrabKey(Window.X11DisplayPtr, _Keycode5, MASK | KeyButtonMask.ShiftMask, _XWindow, false, GrabMode.Async, GrabMode.Async);
 
+		// Xlib.XGrabKey(Window.X11DisplayPtr, _KeycodeLeft, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
+		// Xlib.XGrabKey(Window.X11DisplayPtr, _KeycodeMiddle, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
+		Xlib.XGrabButton(Window.X11DisplayPtr, Button.LEFT, MASK, _XWindow, false, EventMask.ButtonPressMask | EventMask.ButtonReleaseMask, GrabMode.Async, GrabMode.Async, X11.Window.None, FontCursor.None);
+		Xlib.XGrabButton(Window.X11DisplayPtr, Button.RIGHT, MASK, _XWindow, false, EventMask.ButtonPressMask | EventMask.ButtonReleaseMask, GrabMode.Async, GrabMode.Async, X11.Window.None, FontCursor.None);
+		
 		Xlib.XGrabKey(Window.X11DisplayPtr, _KeycodeDisable, MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
 		Xlib.XGrabKey(Window.X11DisplayPtr, _KeycodeEnable, ENABLE_MASK, _XWindow, false, GrabMode.Async, GrabMode.Async);
 
@@ -122,11 +124,11 @@ public static class Program {
 		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode3, MASK, _XWindow);
 		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode4, MASK, _XWindow);
 		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode5, MASK, _XWindow);
-		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode1, MASK | KeyButtonMask.ShiftMask, _XWindow);
-		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode2, MASK | KeyButtonMask.ShiftMask, _XWindow);
-		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode3, MASK | KeyButtonMask.ShiftMask, _XWindow);
-		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode4, MASK | KeyButtonMask.ShiftMask, _XWindow);
-		Xlib.XUngrabKey(Window.X11DisplayPtr, _Keycode5, MASK | KeyButtonMask.ShiftMask, _XWindow);
+
+		Xlib.XUngrabButton(Window.X11DisplayPtr, Button.LEFT, KeyButtonMask.Mod2Mask, _XWindow);
+		Xlib.XUngrabButton(Window.X11DisplayPtr, Button.RIGHT, KeyButtonMask.Mod2Mask, _XWindow);
+		// Xlib.XUngrabKey(Window.X11DisplayPtr, _KeycodeLeft, MASK, _XWindow);
+		// Xlib.XUngrabKey(Window.X11DisplayPtr, _KeycodeMiddle, MASK, _XWindow);
 
 		Xlib.XUngrabKey(Window.X11DisplayPtr, _KeycodeDisable, MASK, _XWindow);
 		Xlib.XUngrabKey(Window.X11DisplayPtr, _KeycodeEnable, ENABLE_MASK, _XWindow);
@@ -140,6 +142,9 @@ public static class Program {
 		_Keycode3 = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.h);
 		_Keycode2 = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.t);
 		_Keycode1 = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.space);
+
+		// _KeycodeLeft = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.KP_Multiply);
+		// _KeycodeMiddle = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.KP_Subtract);
 
 		_KeycodeDisable  = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.g);
 		_KeycodeEnable = Xlib.XKeysymToKeycode(Window.X11DisplayPtr, (KeySym)KeySyms.space);
@@ -161,15 +166,28 @@ public static class Program {
 		int pressedKeys = 0;
 		
 		bool continueRunning = true;
+
+		X11.Window pointerWindow = new();
+		X11.Window pointerChild = new();
+
+		int pointerRootX = 0;
+		int pointerRootY = 0;
+		int pointerWinX = 0;
+		int pointerWinY = 0;
+
+		uint pointerMask = 0;
 		
 		while (continueRunning) {
 			XLibB.QueryKeymap(Window.X11DisplayPtr, queryReturnArr);
+			Xlib.XQueryPointer(Window.X11DisplayPtr, _XWindow, ref pointerWindow, ref pointerChild, ref pointerRootX, ref pointerRootY, ref pointerWinX, ref pointerWinY, ref pointerMask);
 
 			// foreach (byte b in queryReturnArr) {
 			// 	if(b != 0)
 			// 		Debugger.Break();
 			// }
-
+			
+			// Console.WriteLine($"Left: {(pointerMask & (1 << 8)) != 0} Right: {(pointerMask & (1 << 10)) != 0}");
+			
 			SetKeyBit(0, (queryReturnArr[8] & 2)   == 2); //Space
 			SetKeyBit(1, (queryReturnArr[5] & 2)   == 2); //T
 			SetKeyBit(2, (queryReturnArr[5] & 1)   == 1); //H
@@ -177,6 +195,9 @@ public static class Program {
 			SetKeyBit(4, (queryReturnArr[4] & 64)  == 64); //A
 			
 			SetKeyBit(5, (queryReturnArr[4] & 32) == 32); //Control
+			
+			SetKeyBit(7, (pointerMask & (1 << 8)) != 0);  //Left
+			SetKeyBit(6, (pointerMask & (1 << 10)) != 0); //Right
 			
 			Thread.Sleep(5);
 		}
